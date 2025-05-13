fb8718c71f628ba619b2070541ee75b56af162b7
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-05-13 18:16:19 +0300

    Add solution for Counting Bits

--- LeetCode/Counting-Bits/Solution.py
+++ LeetCode/Counting-Bits/Solution.py
@@ -2,6 +2,6 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = []
         for i in range(n+1):
-            temp = bin(i)
-            ans.append(temp.count("1"))
+           
+            ans.append(i.bit_count())
         return ans
