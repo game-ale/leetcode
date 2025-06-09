6cd5ccafc765327e12f3871384e7052abd2e2aa5
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-06-09 22:47:06 +0300

    Time: 598 ms (67.27%), Space: 33.3 MB (7.52%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,34 +1,26 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def mergesort(nums,left,right):
-            if left >= right:
-                return 
-            mid = (right + left)//2
-            mergesort(nums,left,mid)
-            mergesort(nums,mid+1,right)
-            return (merge(nums,left,mid,right))
-        def merge (nums,left, mid ,right):
-            i, j = left, mid+1
-            temp = []
-            while i <= mid and j <= right:
-                if nums[i]<=nums[j]:
-                    temp.append(nums[i])
+        def quicksort(low, high):
+            if low >= high:
+                return
+            pivot_index = random.randint(low, high)
+            nums[high], nums[pivot_index] = nums[pivot_index], nums[high]
+            pivot = nums[high]
+            greater = high
+            i = low
+            less = low
+            while i <= greater:
+                if nums[i] < pivot:
+                    nums[less], nums[i] = nums[i], nums[less]
+                    less += 1
+                    i += 1
+                elif nums[i] ==  pivot:
                     i+=1
                 else:
-                    temp.append(nums[j])
-                    j+=1
-            while i <= mid:
-                temp.append(nums[i])
-                i+=1
-            while j <= right:
-                temp.append(nums[j])
-                j+=1
-            nums[left:right+1] = temp
-            
-        mergesort(nums,0,len(nums)-1)
-        return (nums)
-        
+                    nums[i], nums[greater] = nums[greater], nums[i]
+                    greater -= 1
+            quicksort(low, less- 1)
+            quicksort(greater + 1, high)
 
-
-
-        
+        quicksort(0, len(nums) - 1)
+        return nums
