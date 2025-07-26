31d3a9e29946f7d7ba30b842c331371560beacc3
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-07-26 21:12:22 +0300

    Add solution for 01 Matrix

--- LeetCode/01-Matrix/Solution.py
+++ LeetCode/01-Matrix/Solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(mat), len(mat[0])
-        dist = [[float('inf')] * cols for _ in range(rows)]
+        rows, columns = len(mat), len(mat[0])
+        dist = [[float('inf')] * columns for _ in range(rows)]
         queue = deque()
         for r in range(rows):
-            for c in range(cols):
+            for c in range(columns):
                 if mat[r][c] == 0:
                     dist[r][c] = 0
                     queue.append((r, c))
@@ -13,7 +13,7 @@ class Solution:
             r, c = queue.popleft()
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < rows and 0 <= nc < cols and dist[nr][nc] > dist[r][c] + 1:
+                if 0 <= nr < rows and 0 <= nc < columns and dist[nr][nc] > dist[r][c] + 1:
                     dist[nr][nc] = dist[r][c] + 1
                     queue.append((nr, nc))
         return dist
