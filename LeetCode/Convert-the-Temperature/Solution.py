f998b6e1c18d8f27171bb0ee0617c8b4ad52b4b2
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-09-05 17:59:15 +0300

    Add solution for Convert the Temperature

--- LeetCode/Convert-the-Temperature/Solution.py
+++ LeetCode/Convert-the-Temperature/Solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        Kelvin = celsius + 273.15
-        Fahrenheit = celsius * 1.80 + 32.00
-        return [Kelvin,Fahrenheit]   
-            
+        degre_Kelvin = celsius + 273.15
+        degre_Fahrenheit = celsius * 1.80 + 32.00
+        return [degre_Kelvin,degre_Fahrenheit]
