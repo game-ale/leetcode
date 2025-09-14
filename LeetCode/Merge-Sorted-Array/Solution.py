6733d33007ffd3f943d3aa648875c7cbcb6a99cd
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-09-14 20:34:44 +0300

    Add solution for Merge Sorted Array

--- LeetCode/Merge-Sorted-Array/Solution.py
+++ LeetCode/Merge-Sorted-Array/Solution.py
@@ -3,22 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-          
-
-        i, j, k = m - 1, n - 1, m + n - 1  # Pointers for nums1, nums2, and merged array
-
-        while j >= 0: 
-
-            if i >= 0 and nums1[i] > nums2[j]:
-
-                nums1[k] = nums1[i]
-
-                i -= 1
-
+        p1, p2, p = m - 1, n - 1, m + n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
             else:
-
-                nums1[k] = nums2[j]
-
-                j -= 1
-
-            k -= 1
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
