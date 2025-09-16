44bcabbadbe47765eeb32a55d02a663749f6ffe2
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-09-16 20:32:35 +0300

    Time: 130 ms (60.87%), Space: 31.5 MB (59.78%) - LeetHub

---
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def replaceNonCoprimes(self, nums):
+      
+        stack = []
+        
+       
+        for num in nums:
+            while stack and math.gcd(stack[-1], num) > 1:
+              
+                top = stack.pop()
+                num = (top * num) // math.gcd(top, num)
+            
+           
+            stack.append(num)
+      
+        return stack
