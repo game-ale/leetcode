98638838f3c7597d19029ad6afe0119cfc1c37d4
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-09-21 23:36:10 +0300

    Add solution for Check if All the Integers in a Range Are Covered

--- LeetCode/Check-if-All-the-Integers-in-a-Range-Are-Covered/Solution.py
+++ LeetCode/Check-if-All-the-Integers-in-a-Range-Are-Covered/Solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
-        firstset = set()
-        secondset = set()
+        first = set()
+        second = set()
         for  rang in ranges:
             for j in range(rang[0],rang[1]+1):
-                firstset.add(j)
+                first.add(j)
         for i in range (left,right+1):
-            secondset.add(i)
-        if secondset.issubset(firstset):
+            second.add(i)
+        if second.issubset(first):
             return True
         return False
