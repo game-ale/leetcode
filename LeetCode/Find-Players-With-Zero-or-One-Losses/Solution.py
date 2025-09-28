362b4633d0c69e99c71e279f345be5d03bb614b2
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-09-28 20:42:26 +0300

    Add solution for Find Players With Zero or One Losses

--- LeetCode/Find-Players-With-Zero-or-One-Losses/Solution.py
+++ LeetCode/Find-Players-With-Zero-or-One-Losses/Solution.py
@@ -1,20 +1,15 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
 
-        count = Counter()
+        cnt = Counter()
         ans = [[],[]]
         for m,n in matches:
-            if m not in count:
-                count[m] = 0
-            count[n]+=1
-        for x, y in count.items():
+            if m not in cnt:
+                cnt[m] = 0
+            cnt[n]+=1
+        for x, y in cnt.items():
             if y<2:
                 ans[y].append(x)
         ans[0].sort()
         ans[1].sort()
         return ans
-
-
-
-             
-        
