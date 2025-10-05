5ebd51a1a67e09dafcef58e9007da966293f5ea1
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-10-05 18:56:22 +0300

    Add solution for Keyboard Row

--- LeetCode/Keyboard-Row/Solution.py
+++ LeetCode/Keyboard-Row/Solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
         ans = []
-        first = "qwertyuiop"
-        first = set(first)
+        start = "qwertyuiop"
+        start = set(start)
         second = "asdfghjkl"
         second = set(second)
         third  = "zxcvbnm"
         third = set(third)
         for word in words:
             temp = set(word.lower())
-            if temp.issubset(first) or temp.issubset(second) or temp.issubset(third):
+            if temp.issubset(start) or temp.issubset(second) or temp.issubset(third):
                 ans.append(word)
         return ans
-        
