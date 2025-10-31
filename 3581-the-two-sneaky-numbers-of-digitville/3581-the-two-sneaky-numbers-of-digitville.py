c4af54d73eb9a7dc5035fecf2cd0a3ad5a41a0a2
Gemechu Alemu Bedasa <alemugemechu44@gmail.com>
2025-10-31 13:22:01 +0300

    Time: 3 ms (51.08%), Space: 17.8 MB (40.27%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -1,5 +1,6 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        freq = Counter(nums)
-        top_two = sorted(freq.items(), key=lambda item: item[1], reverse=True)[:2]
-        return [num for num, _ in top_two]
+        cnt = Counter(nums)
+        return [key for key, val in cnt.items() if val>1]
+            
+        
